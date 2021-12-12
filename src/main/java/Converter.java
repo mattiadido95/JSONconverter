25b24bea7aa9cfb771226d218e72e3cdf3fc5154
mattiadido95 <mattia.didonato95@gmail.com>
2021-12-12 19:37:15 +0100

    ultimato convertitore in JSON, inserito limite a 200 elementi

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -13,46 +13,87 @@ import java.util.List;
 
 public class Converter {
     public static void main(String args[]) {
-        //Creating a JSONObject object
-        JSONObject jsonObject = new JSONObject();
-        //Inserting key-value pairs into the json object
-        jsonObject.put("ID", "1");
-        jsonObject.put("First_Name", "Shikhar");
-        jsonObject.put("Last_Name", "Dhawan");
-        jsonObject.put("Date_Of_Birth", "1981-12-05");
-        jsonObject.put("Place_Of_Birth", "Delhi");
-        jsonObject.put("Country", "India");
-        JSONObject jsonObject2 = new JSONObject();
-        //Inserting key-value pairs into the json object
-        jsonObject2.put("ID", "2");
-        jsonObject2.put("First_Name", "Shikhar");
-        jsonObject2.put("Last_Name", "Dhawan");
-        jsonObject2.put("Date_Of_Birth", "1981-12-05");
-        jsonObject2.put("Place_Of_Birth", "Delhi");
-        jsonObject2.put("Country", "India");
-        //put all in array
         JSONArray array = new JSONArray();
-        array.add(jsonObject);
-        array.add(jsonObject2);
         try {
+            List<String> allLines = Files.readAllLines(Paths.get("amazon-meta.txt"));
+            for (String line : allLines) {
+                if (line.indexOf("Id:") != -1) {
+
+                    System.out.println(line);
+                    int index = allLines.indexOf(line);
+                    if (line.equals("Id:   200")) {
+                        break;
+                    }
+
+                    String exitString = allLines.get(index + 2);
+                    if (exitString.equals("  discontinued product") ) {
+                        System.out.println("stringa di errore"+" "+exitString);
+                        continue;
+                    }
+
+                    String[] parts = line.split(":   ");
+                    JSONObject elem = new JSONObject();
+                    elem.put("id", parts[1]);
+
+                    String title = allLines.get(index + 2);
+                    String[] titleSplit = title.split("title: ");
+                    elem.put("title", titleSplit[1]);
+
+                    String group = allLines.get(index + 3);
+                    String[] groupSplit = group.split("group: ");
+                    elem.put("group", groupSplit[1]);
+
+                    String similar = allLines.get(index + 5);
+                    String[] similarSplit = similar.split("similar: ");
+                    String[] similarSplitElems = similarSplit[1].split("  ");
+                    int similarcount = Integer.parseInt(similarSplitElems[0]);
+                    elem.put("count_similar", similarcount);
+                    JSONArray similarList = new JSONArray();
+                    if (similarcount > 0) {
+                        for (int i = 1; i <= similarcount; i++) {
+                            similarList.add(similarSplitElems[i]);
+                        }
+                    }
+                    elem.put("similar", similarList);
+
+                    String categories = allLines.get(index + 6);
+                    String[] categoriesSplit = categories.split("categories: ");
+                    int cat = Integer.parseInt(categoriesSplit[1]);
+                    elem.put("count_categories", cat);
+                    JSONArray categoriesList = new JSONArray();
+                    if (cat > 0) {
+                        for (int i = 1; i <= cat; i++) {
+                            String catelem = allLines.get(index + 6 + i);
+                            categoriesList.add(catelem);
+                        }
+                        elem.put("categories", categoriesList);
+                    }
+
+                    String reviews = allLines.get(index + 6 + cat + 1);
+                    String[] reviewsSplit = reviews.split("reviews: ");
+                    String[] app = reviewsSplit[1].split(": ");
+                    String[] countReview = app[1].split("  ");
+                    int rev = Integer.parseInt(countReview[0]);
+                    elem.put("count_reviews", rev);
+                    JSONArray reviewsList = new JSONArray();
+                    if (rev > 0) {
+                        for (int i = 1; i <= rev; i++) {
+                            String revlem = allLines.get(index + 6 + cat + i + 1);
+                            reviewsList.add(revlem);
+                        }
+                        elem.put("reviews", reviewsList);
+                    }
+
+                    array.add(elem);
+                }
+            }
             FileWriter file = new FileWriter("output.json");
             file.write(array.toJSONString());
             file.close();
-            List<String> allLines = Files.readAllLines(Paths.get("input.txt"));
-            for (String line : allLines) {
-                System.out.println(line);
-                String search = "total: ";
-                if (line.toLowerCase().indexOf(search.toLowerCase()) != -1) {
-                    System.out.println("I found the keyword");
-                } else {
-                    System.out.println("not found");
-                }
-            }
-
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println("JSON file created: " + jsonObject);
+
     }
 }
