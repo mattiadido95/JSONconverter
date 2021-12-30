3adf04c7d7f424cae4384524eb61250db52a823d
mattiadido95 <mattia.didonato95@gmail.com>
2021-12-30 10:40:51 +0100

    implementato web scraping per prelievo authori su sito amazon

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,25 +1,37 @@
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
+import org.apache.commons.lang3.StringUtils;
 
+import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.zip.GZIPInputStream;
 
 public class Converter {
-    public static void main(String args[]) {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "https://www.amazon.in/s?k=0827229534&ref=hp_aps_search";
+
+
+    public static void Convert() {
         JSONArray array = new JSONArray();
         try {
-            List<String> allLines = Files.readAllLines(Paths.get("../amazon-meta.txt"));
+            List<String> allLines = Files.readAllLines(Paths.get("../Materiale/amazon-meta.txt"));
             for (String line : allLines) {
                 if (line.indexOf("Id:") != -1) {
                     JSONObject elem = new JSONObject();
                     System.out.println(line);
                     int index = allLines.indexOf(line);
-                    if (line.equals("Id:   1000")) {
+                    if (line.equals("Id:   300")) {
                         break;
                     }
 
@@ -33,6 +45,11 @@ public class Converter {
                     String[] groupSplit = group.split("group: ");
                     if (groupSplit[1].equals("Book")) {
                         elem.put("group", groupSplit[1]);
+
+                        String asin = allLines.get(index + 1);
+                        String[] asinSplit = asin.split("ASIN: ");
+                        String author = Scrape(asinSplit[1]);
+                        elem.put("author", author);
                     } else {
                         System.out.println("trovato un non libro");
                         continue;
@@ -89,14 +106,68 @@ public class Converter {
                     array.add(elem);
                 }
             }
-            FileWriter file = new FileWriter("output.json");
+            FileWriter file = new FileWriter("../Materiale/datasetAmazon.json");
             file.write(array.toJSONString());
             file.close();
             System.out.println("elementi inseriti nel JSON: " + array.size());
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
     }
+
+    public static String Scrape(String asin) throws IOException {
+        URL obj = new URL("https://www.amazon.in/s?k=" + asin + "&ref=hp_aps_search");
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Encoding", "gzip");
+        int responseCode = con.getResponseCode();
+        // System.out.println("GET Response Code :: " + con.getResponseCode() + "...." + con.getInputStream());
+        Reader reader = null;
+        if ("gzip".equals(con.getContentEncoding())) {
+            reader = new InputStreamReader(new GZIPInputStream(con.getInputStream()));
+        } else {
+            reader = new InputStreamReader(con.getInputStream());
+        }
+        StringBuffer response = new StringBuffer();
+        while (true) {
+            int ch = reader.read();
+            if (ch == -1) {
+                break;
+            }
+            response.append((char) ch);
+        }
+        //System.out.println(response);
+        String str = response.toString();
+        String result = StringUtils.substringBetween(str, "<span class=\"a-size-base\">by </span>", "</div>");
+        System.out.println("risultato prima botta \n" + result);
+        if (StringUtils.substringBetween(result, ">", "</a>") != null) {
+            if (StringUtils.substringBetween(result, ">", "</a>").contains("<")) {
+                String app[] = StringUtils.substringBetween(result, ">", "</a>").split("<");
+                System.out.println(app[0]);
+                return app[0];
+            } else {
+                System.out.println(StringUtils.substringBetween(result, ">", "</a>"));
+                return StringUtils.substringBetween(result, ">", "</a>");
+            }
+        } else if (StringUtils.substringBetween(result, ">", "</span>") != null) {
+            if (StringUtils.substringBetween(result, ">", "</span>").contains("<")) {
+                String app[] = StringUtils.substringBetween(result, ">", "</span>").split("<");
+                System.out.println(app[0]);
+                return app[0];
+            } else {
+                System.out.println(StringUtils.substringBetween(result, ">", "</span>"));
+                return StringUtils.substringBetween(result, ">", "</span>");
+            }
+        }
+
+        return "";
+    }
+
+    public static void main(String[] args) throws IOException {
+        Convert();
+        //Scrape();
+    }
+
+
 }
